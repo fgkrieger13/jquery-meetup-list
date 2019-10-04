1b2c06693890b8fae2dff5887797a5463e4b0526
Grant Krieger <fgkrieger13@gmail.com>
2019-10-04 13:07:26 -0500

    adds appended values to the dom and delete button responds *except for newly created values

--- client.js
+++ client.js
@@ -1,10 +1,27 @@
 $(document).ready(readyNow);
 
+let numberMeetups = 3
+
 function readyNow(){
     console.log('JQ');
-   const totalMeetups = ('')
-   $('#meetup').on('click', function(){
-       console.log('click');
-      let group = $('#button');
-   })
+
+   $('#meetup').on('click', addMeetup);
+   $('.delete').on('click', deleteFunction);
+   
+    
+}
+
+function addMeetup(){
+    console.log('click');
+    console.log($('#group').val());
+    console.log($('#date').val());
+    $('table').append('<tr>' + '<td>' + ($('#group').val()) +'</td>' + '<td>' + ($('#date').val()) + '</td>' + '<td>' + "<button class = 'delete'> delete </button>" + '</td>' + '</tr>')
+    numberMeetups = Number(numberMeetups) + 1;
+    $('#number').text(numberMeetups);
+}
+
+function deleteFunction(){
+    console.log('delete');
+    
+    $('tr').closest.remove;
 }
